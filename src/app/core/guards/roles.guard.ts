a423cd092412dbd1b7213ea8f7e1b17cea26a92f
shehara_r <ranathisarashehara01@gmail.com>
2025-02-25 20:52:37 +0530

    feat(guards): jwt + role guard

---
+++ src/app/core/guards/roles.guard.ts
@@ -0,0 +1,30 @@
+import {
+  Injectable,
+  ExecutionContext,
+  UnauthorizedException,
+} from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { AuthGuard } from "@nestjs/passport";
+
+import { UserRole } from "@features/users/entities/user.entity";
+import { Request } from "@shared/models/Request";
+
+@Injectable()
+export class RolesGuard extends AuthGuard("jwt") {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext): Promise<boolean> | boolean {
+    const roles = this.reflector.get<string[]>("roles", context.getHandler());
+    if (!roles) {
+      return true;
+    }
+    const request: Request = context.switchToHttp().getRequest();
+
+    if (!request.headers.authorization) throw new UnauthorizedException();
+    else {
+      return roles.includes(request.user?.role as UserRole);
+    }
+  }
+}
